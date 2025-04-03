a3c22ed19853de0fb1134518e189bbbb55b5e969
Ayrtom-S <ayrtoncarlos@gmail.com>
2025-04-02 20:29:50 -0400

    11-11-implementar-autenticacao-e-seguranca-com-jwt

--- src/main/java/edu/uea/dsw/api_pagamentos/controller/LancamentoController.java
+++ src/main/java/edu/uea/dsw/api_pagamentos/controller/LancamentoController.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class LancamentoController {
 
     // GET /lancamentos
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<Page<LancamentoDTO>> pesquisar(LancamentoFilterDTO lancamentoFilter, Pageable pageable) {
         Page<LancamentoDTO> lancamentos = lancamentoService.pesquisar(lancamentoFilter, pageable);
         return ResponseEntity.ok(lancamentos);
@@ -40,6 +42,7 @@ public class LancamentoController {
 
     // GET /lancamentos/resumo
     @GetMapping("/resumo")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<Page<ResumoLancamentoDTO>> resumir(LancamentoFilterDTO lancamentoFilter, Pageable pageable) {
         Page<ResumoLancamentoDTO> lancamentos = lancamentoService.resumir(lancamentoFilter, pageable);
         return ResponseEntity.ok(lancamentos);
@@ -47,6 +50,7 @@ public class LancamentoController {
 
     // GET /lancamentos/{codigo}
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<LancamentoDTO> buscarLancamento(@PathVariable Long codigo) {
         LancamentoDTO lancamento = lancamentoService.buscarLancamentoPorCodigo(codigo);
         return ResponseEntity.ok(lancamento);
@@ -54,6 +58,7 @@ public class LancamentoController {
 
     // POST /lancamentos
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<LancamentoDTO> criarLancamento(@Valid @RequestBody LancamentoDTO lancamentoDTO) {
         LancamentoDTO lancamentoCriado = lancamentoService.criarLancamento(lancamentoDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -65,6 +70,7 @@ public class LancamentoController {
 
     // PUT /lancamentos/{codigo}
     @PutMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<LancamentoDTO> atualizarLancamento(@Valid @PathVariable Long codigo,
             @RequestBody LancamentoDTO lancamentoDTO) {
         LancamentoDTO lancamentoAtualizado = lancamentoService.atualizarLancamento(codigo, lancamentoDTO);
@@ -73,6 +79,7 @@ public class LancamentoController {
 
     // DELETE /lancamentos/{codigo}
     @DeleteMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletarLancamento(@PathVariable Long codigo) {
         lancamentoService.deletarLancamento(codigo);
         return ResponseEntity.noContent().build();
